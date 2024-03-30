bad5e6cdf51d633e27b2ce8d628eaca4235325cb
runweiting <whatisfuntime@gmail.com>
2024-03-30 19:05:28 +0800

    update 2024-03-30 19:05:28

--- assets/showErrorToast-8bqWWuy4.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./index-xBDQSGV2.js";function n(o){return new Promise(r=>{e.mixin({toast:!0,position:"bottom-end",showConfirmButton:!1,timer:3e3,timerProgressBar:!1,didOpen:t=>{t.addEventListener("mouseenter",e.stopTimer),t.addEventListener("mouseleave",e.resumeTimer)}}).fire({icon:"error",title:o}).then(()=>{r()})})}export{n as s};
