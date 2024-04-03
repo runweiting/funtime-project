c225484470dd074fba026580d05429ef46d31fd7
runweiting <whatisfuntime@gmail.com>
2024-04-03 23:51:22 +0800

    update 2024-04-03 23:51:22

---
+++ assets/showErrorToast-RmEj-76d.js
@@ -0,0 +1,1 @@
+import{S as e}from"./index-8_2dSobz.js";function n(o){return new Promise(r=>{e.mixin({toast:!0,position:"bottom-end",showConfirmButton:!1,timer:3e3,timerProgressBar:!1,didOpen:t=>{t.addEventListener("mouseenter",e.stopTimer),t.addEventListener("mouseleave",e.resumeTimer)}}).fire({icon:"error",title:o}).then(()=>{r()})})}export{n as s};
