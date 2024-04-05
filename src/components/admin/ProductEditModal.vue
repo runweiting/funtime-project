ab730afd8f6fac3498fd0fae33d6e6e33111e000
runweiting <whatisfuntime@gmail.com>
2024-04-05 08:25:28 +0800

    feat(ProductEditModal, OrderModal, CouponModal, ArticleModal): 移除編輯按鈕

--- src/components/admin/ProductEditModal.vue
+++ src/components/admin/ProductEditModal.vue
@@ -1,24 +1,15 @@
 <template>
-  <div
-    class="modal fade"
-    tabindex="-1"
-    aria-labelledby="editModalLabel"
-    aria-hidden="true"
-    ref="modal"
-  >
+  <div class="modal fade" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true" ref="modal">
     <div class="modal-dialog modal-xl modal-dialog-centered">
       <div class="modal-content">
         <div class="modal-header bg-dark opacity-75 text-white">
           <h5 class="modal-title" id="editModalLabel">
-            <span v-if="isNew">新增專案</span>
-            <span v-else>編輯專案 ID:{{ selectedProduct.id }}</span>
+            <span v-if="isNew">新增商品</span>
+            <span v-else>編輯商品編號：{{ selectedProduct.id }}</span>
           </h5>
-          <button @click="cancelUpdate"
-            type="button"
-            class="btn btn-outline-light"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ><i class="bi bi-x-lg"></i></button>
+          <button @click="cancelUpdate" type="button" class="btn btn-outline-light" data-bs-dismiss="modal" aria-label="Close">
+            <i class="bi bi-x-lg"></i>
+          </button>
         </div>
         <div class="modal-body">
           <div class="container-fluid">
@@ -26,22 +17,13 @@
               <div class="col-md-3">
                 <h5>專案內容</h5>
               </div>
-              <div class="col-md-9">
-                <button @click="togglerEdit" type="button" class="btn btn-warning">編輯商品</button>
-              </div>
             </div>
             <div class="row">
               <!-- 圖片 -->
               <div class="col-md-3">
                 <div class="mb-4">
                   <label for="imgUrl" class="form-label">主要圖片</label>
-                  <input
-                    v-model="selectedProduct.imageUrl" :disabled="isInputEnabled" 
-                    type="url"
-                    class="form-control mb-2"
-                    id="imgUrl"
-                    placeholder="請輸入網址"
-                  />
+                  <input v-model="selectedProduct.imageUrl" type="url" class="form-control mb-2" id="imgUrl" placeholder="請輸入網址"/>
                   <img :src="selectedProduct.imageUrl" class="img-fluid" />
                 </div>
                 <!-- 多圖新增 -->
@@ -49,20 +31,9 @@
                 <!-- // ? v-if 資料是否已有多圖，有則顯示，沒有不顯示 -->
                 <div v-if="Array.isArray(selectedProduct.imagesUrl)" class="mb-4">
                   <div v-for="(imgUrl, index) in selectedProduct.imagesUrl" :key="`url${index}`">
-                    <label :for="`url${index}`" class="form-label"
-                      >圖片網址</label
-                    >
-                    <input
-                      :id="`url${index}`"
-                      v-model="selectedProduct.imagesUrl[index]" :disabled="isInputEnabled" 
-                      type="url"
-                      class="form-control mb-2"
-                      placeholder="請輸入網址"
-                    />
-                    <img
-                      :src="selectedProduct.imagesUrl[index]"
-                      class="img-fluid mb-2"
-                    />
+                    <label :for="`url${index}`" class="form-label">圖片網址</label>
+                    <input :id="`url${index}`" v-model="selectedProduct.imagesUrl[index]"  type="url" class="form-control mb-2" placeholder="請輸入網址"/>
+                    <img :src="selectedProduct.imagesUrl[index]" class="img-fluid mb-2" />
                   </div>
                   <div class="d-flex gap-2">
                     <!--
@@ -80,11 +51,7 @@
                     >
                       新增圖片
                     </button>
-                    <button
-                      @click="selectedProduct.imagesUrl.pop()"
-                      type="button"
-                      class="btn btn-danger text-white w-100"
-                    >
+                    <button @click="selectedProduct.imagesUrl.pop()" type="button" class="btn btn-danger text-white w-100">
                       刪除圖片
                     </button>
                   </div>
@@ -92,13 +59,7 @@
                 <!-- 上傳圖片 -->
                 <h4 class="fw-bold">上傳圖片</h4>
                 <div class="mb-4">
-                  <input
-                    @change="upload" :disabled="isInputEnabled" 
-                    class="form-control"
-                    type="file"
-                    id="formFile"
-                    placeholder="請輸入圖片連結"
-                  />
+                  <input @change="upload" class="form-control" type="file" id="formFile" placeholder="請輸入圖片連結"/>
                 </div>
               </div>
               <!-- 內容 -->
@@ -107,53 +68,30 @@
                 <div class="row">
                   <div class="col-8">
                     <label for="title" class="form-label">完整標題</label>
-                    <input
-                      v-model="selectedProduct.title" :disabled="isInputEnabled" 
-                      type="text"
-                      class="form-control mb-4"
-                      name="full-title"
-                      placeholder="請輸入完整標題"
-                    />
+                    <input v-model="selectedProduct.title" type="text" class="form-control mb-4" name="full-title" placeholder="請輸入完整標題"/>
                   </div>
                   <div class="col-4">
                     <label for="short-title" class="form-label">簡短標題</label>
-                    <input
-                      v-model="selectedProduct.short_title" :disabled="isInputEnabled" 
-                      type="text"
-                      class="form-control mb-4"
-                      name="short-title"
-                      placeholder="請輸入簡單標題"
-                    />
+                    <input v-model="selectedProduct.short_title" type="text" class="form-control mb-4" name="short-title" placeholder="請輸入簡單標題"/>
                   </div>
                 </div>
                 <!-- 描述 -->
                 <div class="row mb-4">
                   <div class="col">
                     <label for="description" class="form-label">專案描述</label>
-                    <textarea
-                      v-model="selectedProduct.description" :disabled="isInputEnabled" 
-                      class="form-control"
-                      placeholder="請輸入專案描述"
-                      id="description" rows="3"
-                    ></textarea>
+                    <textarea v-model="selectedProduct.description" class="form-control" placeholder="請輸入專案描述" id="description" rows="3"></textarea>
                   </div>
                 </div>
                 <!-- 提案人、#標籤、特色 -->
                 <div class="row mb-4">
                   <div class="col">
                     <label for="proposer" class="form-label">提案人</label>
-                    <input
-                      v-model="selectedProduct.proposer" :disabled="isInputEnabled" 
-                      type="text"
-                      class="form-control"
-                      id="unit"
-                      placeholder="請輸入名稱"
-                    />
+                    <input v-model="selectedProduct.proposer" type="text" class="form-control" id="unit" placeholder="請輸入名稱"/>
                   </div>
                   <div class="col">
                     <label for="tags" class="form-label">標籤</label>
                     <div class="input-group mb-2">
-                      <input type="text" class="form-control" placeholder="請輸入標籤" v-model="newTag" @keyup.enter="addTag" :disabled="isInputEnabled">
+                      <input type="text" class="form-control" placeholder="請輸入標籤" v-model="newTag" @keyup.enter="addTag">
                       <button class="btn btn-gray text-white" type="button" @click="addTag">新增</button>
                     </div>
                     <div class="d-flex flex-wrap gap-2">
@@ -167,7 +105,7 @@
                   <div class="col">
                     <label for="features" class="form-label">特色</label>
                     <div class="input-group mb-2">
-                      <input type="text" class="form-control" placeholder="請輸入特色" v-model="newFeature" @keyup.enter="addFeature" :disabled="isInputEnabled">
+                      <input type="text" class="form-control" placeholder="請輸入特色" v-model="newFeature" @keyup.enter="addFeature">
                       <button class="btn btn-gray text-white" type="button" @click="addFeature">新增</button>
                     </div>
                     <div class="d-flex flex-wrap gap-2">
@@ -183,7 +121,7 @@
                 <div class="row">
                   <div class="col">
                     <label for="category" class="form-label">分類</label>
-                    <select v-model="selectedProduct.category" :disabled="isInputEnabled" class="form-select" aria-label="category"  id="category" name="category">
+                    <select v-model="selectedProduct.category" class="form-select" aria-label="category"  id="category" name="category">
                       <option value="" disabled selected hidden>請輸入分類</option>
                       <option value="地圖">地圖</option>
                       <option value="卡牌">卡牌</option>
@@ -193,24 +131,12 @@
                   </div>
                   <div class="col">
                     <label for="target_units" class="form-label">目標組數</label>
-                    <input
-                      v-model.number="selectedProduct.target_units" :disabled="isInputEnabled" 
-                      type="number"
-                      min="0"
-                      class="form-control"
-                      id="target_units"
-                      placeholder="請輸入目標組數"
+                    <input v-model.number="selectedProduct.target_units" type="number" min="0" class="form-control" id="target_units" placeholder="請輸入目標組數"
                     />
                   </div>
                   <div class="col">
                     <label for="unit" class="form-label">單位</label>
-                    <input
-                      v-model="selectedProduct.unit" :disabled="isInputEnabled" 
-                      type="text"
-                      class="form-control"
-                      id="unit"
-                      placeholder="請輸入單位"
-                    />
+                    <input v-model="selectedProduct.unit" type="text" class="form-control" id="unit" placeholder="請輸入單位"/>
                   </div>
                 </div>
                 <hr class="w-100 border-top my-8" style="border: 3px dotted #8C8C8E;">
@@ -218,42 +144,25 @@
                 <div class="row mb-4">
                   <div class="col">
                     <label for="content_question" class="form-label">困擾標題</label>
-                    <input
-                      v-model="selectedProduct.question" :disabled="isInputEnabled" 
-                      type="text"
-                      class="form-control"
-                      id="content_question"
-                      placeholder="請輸入困擾標題"
-                    />
+                    <input v-model="selectedProduct.question" type="text" class="form-control" id="content_question" placeholder="請輸入困擾標題"/>
                   </div>
                 </div>
                 <!-- 設計特點 -->
                 <div class="row mb-4">
                   <div class="col-4">
                     <label for="point-title" class="form-label">設計特點</label>
-                    <input type="text" class="form-control" placeholder="請輸入特點" v-model="newPoint.title" :disabled="isInputEnabled">
+                    <input type="text" class="form-control" placeholder="請輸入特點" v-model="newPoint.title">
                   </div>
                   <div class="col-8">
                     <label for="point-content" class="form-label">設計內容</label>
-                      <textarea
-                        v-model="newPoint.content" :disabled="isInputEnabled" 
-                        class="form-control"
-                        placeholder="請輸入內容"
-                        id="point-content" rows="1"
-                      ></textarea>
+                      <textarea v-model="newPoint.content" class="form-control" placeholder="請輸入內容" id="point-content" rows="1"></textarea>
                   </div>
                 </div>
                 <!-- 情境圖片 -->
                 <div class="row align-items-center mb-4">
                   <div class="col">
                     <label for="point-imgUrl" class="form-label">情境圖片</label>
-                    <input 
-                      v-model="newPoint.imageUrl" :disabled="isInputEnabled" 
-                      type="url"
-                      class="form-control"
-                      id="point-imgUrl"
-                      placeholder="請輸入網址"
-                    />
+                    <input  v-model="newPoint.imageUrl" type="url" class="form-control" id="point-imgUrl" placeholder="請輸入網址"/>
                   </div>
                   <div class="col">
                     <img :src="newPoint.imageUrl" class="img-fluid" />
@@ -283,45 +192,26 @@
                 <div class="row mb-4">
                   <div class="col">
                     <label for="original-price" class="form-label">售價</label>
-                    <input
-                      v-model.number="selectedProduct.origin_price" :disabled="isInputEnabled" 
-                      type="number"
-                      min="0"
-                      class="form-control"
-                      id="original-price"
-                      placeholder="請輸入售價"
-                    />
+                    <input v-model.number="selectedProduct.origin_price" type="number" min="0" class="form-control" id="original-price" placeholder="請輸入售價"/>
                   </div>
                   <div class="col">
                     <label for="discount" class="form-label">折扣</label>
-                    <input @keyup="calculatePrice"
-                      v-model.number="selectedProduct.discount" :disabled="isInputEnabled" 
-                      type="number"
-                      min="0"
-                      class="form-control"
-                      id="discount"
-                      placeholder="請輸入折扣"
-                    />
+                    <input @keyup="calculatePrice" v-model.number="selectedProduct.discount" type="number" min="0" class="form-control" id="discount" placeholder="請輸入折扣"/>
                   </div>
                   <div class="col">
                     <label for="original-price" class="form-label">折扣價</label>
-                    <input
-                      v-model.number="selectedProduct.price" disabled 
-                      type="number"
-                      class="form-control"
-                      id="original-price"
-                    />
+                    <input v-model.number="selectedProduct.price" disabled  type="number" class="form-control" id="original-price"/>
                   </div>
                 </div>
                 <!-- 優惠套裝 -->
                 <div class="row mb-4">
                   <div class="col">
                     <label for="package-name" class="form-label">套裝名稱</label>
-                    <input type="text" class="form-control" placeholder="請輸入名稱" v-model="newPackage.name" :disabled="isInputEnabled">
+                    <input type="text" class="form-control" placeholder="請輸入名稱" v-model="newPackage.name">
                   </div>
                   <div class="col">
                     <label for="package-units" class="form-label">內含組數</label>
-                    <input type="number" min="0" class="form-control" placeholder="請輸入組數" v-model.number="newPackage.units" :disabled="isInputEnabled">
+                    <input type="number" min="0" class="form-control" placeholder="請輸入組數" v-model.number="newPackage.units">
                   </div>
                   <div class="col mt-auto">
                     <button class="btn btn-gray text-white" type="button" @click="addPackage">新增</button>
@@ -347,14 +237,14 @@
                   <div class="col">
                     <label for="contents" class="form-label">內容物</label>
                     <div class="input-group">
-                      <input type="text" class="form-control" placeholder="請輸入內容物" v-model="newContent" :disabled="isInputEnabled">
+                      <input type="text" class="form-control" placeholder="請輸入內容物" v-model="newContent">
                       <button class="btn btn-gray text-white" type="button" @click="addContent">新增</button>
                     </div>
                   </div>
                   <div class="col">
                     <label for="notes" class="form-label">備註</label>
                     <div class="input-group">
-                      <input type="text" class="form-control" placeholder="請輸入備註" v-model="newNote" :disabled="isInputEnabled">
+                      <input type="text" class="form-control" placeholder="請輸入備註" v-model="newNote">
                       <button class="btn btn-gray text-white" type="button" @click="addNote">新增</button>
                     </div>
                   </div>
@@ -391,21 +281,14 @@
                     <span class="ms-2 text-primary fw-bold fs-5">
                       {{ selectedProduct.productRatings }} 分
                     </span>
-                    <input
-                      v-model="selectedProduct.productRatings" :disabled="isInputEnabled" 
-                      type="range"
-                      class="form-range"
-                      min="0"
-                      max="5"
-                      step="1"
-                    />
+                    <input v-model="selectedProduct.productRatings"  type="range" class="form-range" min="0" max="5" step="1"/>
                   </div>
                   <div class="col mt-auto">
                     <div class="form-check pb-2">
                       <label class="form-label m-0" for="product-active">啟用商品</label>
                       <label class="switch ms-2">
                         <!-- <input> 開關、<span> 滑動圓點 -->
-                        <input v-model="selectedProduct.is_enabled" :disabled="isInputEnabled" type="checkbox">
+                        <input v-model="selectedProduct.is_enabled" type="checkbox">
                         <span class="slider"></span>
                       </label>
                     </div>
@@ -469,8 +352,6 @@ export default {
       },
       newContent: '',
       newNote: '',
-      // for toggleEdit
-      isInputEnabled: true,
     };
   },
   created() {
@@ -561,14 +442,7 @@ export default {
     removeNote(index) {
       this.selectedProduct.notes.splice(index, 1);
     },
-    // 修改訂單
-    togglerEdit() {
-      // 啟用或禁用輸入欄位
-      this.isInputEnabled = !this.isInputEnabled;
-    },
-    // 取消編輯
     cancelUpdate() {
-      this.isInputEnabled = true;
       this.selectedProduct = {};
     },
   },
