f082eef5b945c7a42fca167f09898a81f039e13d
runweiting <whatisfuntime@gmail.com>
2024-04-10 16:15:21 +0800

    update 2024-04-10 16:15:21

--- assets/showSuccessToast-cAGz1xrC.js
+++
@@ -1,1 +0,0 @@
-import{S as e}from"./index-B3z9oC5M.js";function n(t){return new Promise(o=>{e.mixin({toast:!0,position:"bottom-end",showConfirmButton:!1,timer:3e3,timerProgressBar:!1,didOpen:s=>{s.addEventListener("mouseenter",e.stopTimer),s.addEventListener("mouseleave",e.resumeTimer)}}).fire({icon:"success",title:t}).then(()=>{o()})})}export{n as s};
